093d4681406e2aee5e340250e9c7d538ba18f40f
Helton-Pereira <helton.bfsm@gmail.com>
2023-02-14 15:59:51 -0300

    Req 05

--- src/Routes/Routes.ts
+++ src/Routes/Routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import CarController from '../Controllers/CarController';
+import MotorcycleController from '../Controllers/MotorcycleController';
 
 const routes = Router();
 
 routes.post(
   '/cars',
   (req, res, next) => new CarController(req, res, next).create(),
+);
+
+routes.post(
+  '/motorcycles',
+  (req, res, next) => new MotorcycleController(req, res, next).create(),
 );
 
 routes.put(
